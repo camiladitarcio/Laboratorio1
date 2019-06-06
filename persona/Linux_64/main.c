1394c45f1921ba014f56b3725b53c700e64acd9a
camiladitarcio <34756590+camiladitarcio@users.noreply.github.com>
2019-06-05 23:10:42 -0300

    Update main.c

--- persona/Linux_64/main.c
+++ persona/Linux_64/main.c
@@ -20,15 +20,45 @@
 
 int main()
 {
-    int option = 0;
+    int opcionMenu;
     LinkedList* listaEmpleados = ll_newLinkedList();
-    do{
-        switch(option)
+   
+        do{
+        utn_getSignedNumber("
+        1)\nAbrir archivo modo texto
+        2)\nAbrir archivo modo binario
+        3)\nAgregar 
+        4)\nModificar
+        5)\nDar de baja
+        6)\nListar
+        7)\nOrdenar
+        8)\nGuardar(modo texto)
+        9)\nGuardar(modo binario)
+       10)\nSalir","\n--ERROR--",
+       1,10,3,&opcionMenu);
+        switch(opcionMenu)
         {
             case 1:
                 controller_loadFromText("data.csv",listaEmpleados);
                 break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                 break;
+            case 5:
+                 break;
+            case 6:
+                 break;
+            case 7:
+                 break;
+            case 8:
+                 break;
+            case 9:
+                 break;
         }
-    }while(option != 10);
+    }while(opcionMenu!=10);
+
     return 0;
 }
