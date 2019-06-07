fc02327af0d128bce1397d3c0baef19abf8b1ebe
camiladitarcio <34756590+camiladitarcio@users.noreply.github.com>
2019-06-06 23:11:54 -0300

    Update Employee.h

--- persona/Linux_64/Employee.h
+++ persona/Linux_64/Employee.h
@@ -12,8 +12,6 @@ Employee* employee_new();
 Employee* employee_newParametros(char* idStr,char* nombreStr,char* horasTrabajadasStr,char* sueldo);
 Employee* employee_newParametrosInt(int idStr,char* nombreStr,int horasTrabajadasStr,int sueldo);
 void employee_delete(Employee* this);
-int employee_buscarLibre(Employee* array[], int size, int* posicion);
-int employee_generarID(void);
 
 int employee_setId(Employee* this,int id);
 int employee_setIdString(Employee* this,char* id);
